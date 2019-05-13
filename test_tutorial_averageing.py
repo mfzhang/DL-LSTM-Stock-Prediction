9cbed2f8b3963f6e0e0eb23fcbaadf0013b5557c
jeff-maes <jeffmaes.jm@gmail.com>
2019-05-13 10:21:24 +0200

    data acquisition

--- test_tutorial_averageing.py
+++ test_tutorial_averageing.py
@@ -9,176 +9,132 @@ import numpy as np
 import tensorflow as tf # This code has been tested with TensorFlow 1.6
 from sklearn.preprocessing import MinMaxScaler
 
-##################################################### switches
-plot_stock = 0      # original stock data plot, 0: no plot, 1: plot
+##################################################### plot switches
+plot_stock = 0                  # original stock data plot, 0: no plot, 1: plot
 plot_standard_average = 0       # standard average plot, 0: no plot, 1: plot
 plot_moving_average = 0         # moving average plot, 0: no plot, 1: plot
 
 ##################################################### Getting data
-data_source = 'kaggle' # alphavantage or kaggle
-
-if data_source == 'alphavantage':
-    # ====================== Loading Data from Alpha Vantage ==================================
-
-    api_key = '01E2Q0VLPBHJWOS9'
-
-    # American Airlines stock market prices
-    ticker = "AAL"
-
-    # JSON file with all the stock market data for AAL from the last 20 years
-    url_string = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=%s&outputsize=full&apikey=%s"%(ticker,api_key)
-
-    # Save data to this file
-    file_to_save = 'stock_market_data-%s.csv'%ticker
-    
-    # If you haven't already saved data,
-    # Go ahead and grab the data from the url
-    # And store date, low, high, volume, close, open values to a Pandas DataFrame
-    if not os.path.exists(file_to_save):
-        with urllib.request.urlopen(url_string) as url:
-            data = json.loads(url.read().decode())
-            # extract stock market data
-            data = data['Time Series (Daily)']
-            df = pd.DataFrame(columns=['Date','Low','High','Close','Open'])
-            for k,v in data.items():
-                date = dt.datetime.strptime(k, '%Y-%m-%d')
-                data_row = [date.date(),float(v['3. low']),float(v['2. high']),
-                            float(v['4. close']),float(v['1. open'])]
-                df.loc[-1,:] = data_row
-                df.index = df.index + 1
-        print('Data saved to : %s'%file_to_save)        
-        df.to_csv(file_to_save)
-
-    # If the data is already there, just load it from the CSV
-    else:
-        print('File already exists. Loading data from CSV')
-        df = pd.read_csv(file_to_save)
-        
-else:
-
-    # ====================== Loading Data from Kaggle ==================================
-    # You will be using HP's data. Feel free to experiment with other data.
-    # But while doing so, be careful to have a large enough dataset and also pay attention to the data normalization
-    df = pd.read_csv(os.path.join('Stocks','hpq.us.txt'),delimiter=',',usecols=['Date','Open','High','Low','Close'])
-    print('Loaded data from the Kaggle repository')
+df = pd.read_csv(os.path.join('data/stock_data/AEX/AALB.csv'),delimiter=',',usecols=['Date','Open','High','Low','Close'])
+print('Loaded data from the AEX repository')
     
 ############################################################### data exploration
 # Sort DataFrame by date
 df = df.sort_values('Date')
 
-############################################################## data visualisation
-
-if plot_stock == 1:
-    plt.figure(figsize = (18,9))
-    plt.plot(range(df.shape[0]),(df['Low']+df['High'])/2.0)
-    plt.xticks(range(0,df.shape[0],500),df['Date'].loc[::500],rotation=45)
-    plt.xlabel('Date',fontsize=18)
-    plt.ylabel('Mid Price',fontsize=18)
-    plt.show()
-
-############################################################# split in train and test
-# First calculate the mid prices from the highest and lowest
-high_prices = df.loc[:,'High'].as_matrix()
-low_prices = df.loc[:,'Low'].as_matrix()
-mid_prices = (high_prices+low_prices)/2
-
-train_data = mid_prices[:11000]                 # first 11,000 data points
-test_data = mid_prices[11000:]                  # the rest will be test d
-
-############################################################### normalisation
-# Scale the data to be between 0 and 1
-# When scaling remember! You normalize both test and train data with respect to training data
-# Because you are not supposed to have access to test data
-scaler = MinMaxScaler()
-train_data = train_data.reshape(-1,1)
-test_data = test_data.reshape(-1,1)
-
-# Train the Scaler with training data and smooth data
-smoothing_window_size = 2500
-for di in range(0,10000,smoothing_window_size):
-    scaler.fit(train_data[di:di+smoothing_window_size,:])
-    train_data[di:di+smoothing_window_size,:] = scaler.transform(train_data[di:di+smoothing_window_size,:])
-
-# You normalize the last bit of remaining data
-scaler.fit(train_data[di+smoothing_window_size:,:])
-train_data[di+smoothing_window_size:,:] = scaler.transform(train_data[di+smoothing_window_size:,:])
-
-# Reshape both train and test data
-train_data = train_data.reshape(-1)
-
-# Normalize test data
-test_data = scaler.transform(test_data).reshape(-1)
-
-# Now perform exponential moving average smoothing on training data
-# So the data will have a smoother curve than the original ragged data
-EMA = 0.0
-gamma = 0.1
-for ti in range(11000):
-  EMA = gamma*train_data[ti] + (1-gamma)*EMA
-  train_data[ti] = EMA
-
-# Used for visualization and test purposes
-all_mid_data = np.concatenate([train_data,test_data],axis=0)
-
-############################################################# averaging techniques
-########################################################## standard average
-window_size = 100
-N = train_data.size
-std_avg_predictions = []
-std_avg_x = []
-mse_errors = []
-
-for pred_idx in range(window_size,N):
-
-    if pred_idx >= N:
-        date = dt.datetime.strptime(k, '%Y-%m-%d').date() + dt.timedelta(days=1)
-    else:
-        date = df.loc[pred_idx,'Date']
-
-    std_avg_predictions.append(np.mean(train_data[pred_idx-window_size:pred_idx]))
-    mse_errors.append((std_avg_predictions[-1]-train_data[pred_idx])**2)
-    std_avg_x.append(date)
-
-print('MSE error for standard averaging: %.5f'%(0.5*np.mean(mse_errors)))
-
-if plot_standard_average == 1:
-    plt.figure(figsize = (18,9))
-    plt.plot(range(df.shape[0]),all_mid_data,color='b',label='True')
-    plt.plot(range(window_size,N),std_avg_predictions,color='orange',label='Prediction')
-    #plt.xticks(range(0,df.shape[0],50),df['Date'].loc[::50],rotation=45)
-    plt.xlabel('Date')
-    plt.ylabel('Mid Price')
-    plt.legend(fontsize=18)
-    plt.show()
-
-######################################################## exponential moving average
-window_size = 100
-N = train_data.size
-
-run_avg_predictions = []
-run_avg_x = []
-
-mse_errors = []
-
-running_mean = 0.0
-run_avg_predictions.append(running_mean)
-
-decay = 0.5
-
-for pred_idx in range(1,N):
-
-    running_mean = running_mean*decay + (1.0-decay)*train_data[pred_idx-1]
-    run_avg_predictions.append(running_mean)
-    mse_errors.append((run_avg_predictions[-1]-train_data[pred_idx])**2)
-    run_avg_x.append(date)
-
-if plot_moving_average == 1:
-    plt.figure(figsize = (18,9))
-    plt.plot(range(df.shape[0]),all_mid_data,color='b',label='True')
-    plt.plot(range(0,N),run_avg_predictions,color='orange', label='Prediction')
-    #plt.xticks(range(0,df.shape[0],50),df['Date'].loc[::50],rotation=45)
-    plt.xlabel('Date')
-    plt.ylabel('Mid Price')
-    plt.legend(fontsize=18)
-    plt.show()
-
+############################################################### data visualisation
+#
+#if plot_stock == 1:
+#    plt.figure(figsize = (18,9))
+#    plt.plot(range(df.shape[0]),(df['Low']+df['High'])/2.0)
+#    plt.xticks(range(0,df.shape[0],500),df['Date'].loc[::500],rotation=45)
+#    plt.xlabel('Date',fontsize=18)
+#    plt.ylabel('Mid Price',fontsize=18)
+#    plt.show()
+#
+############################################################## split in train and test
+## First calculate the mid prices from the highest and lowest
+#high_prices = df.loc[:,'High'].as_matrix()
+#low_prices = df.loc[:,'Low'].as_matrix()
+#mid_prices = (high_prices+low_prices)/2
+#
+#train_data = mid_prices[:11000]                 # first 11,000 data points
+#test_data = mid_prices[11000:]                  # the rest will be test d
+#
+################################################################ normalisation
+## Scale the data to be between 0 and 1
+## When scaling remember! You normalize both test and train data with respect to training data
+## Because you are not supposed to have access to test data
+#scaler = MinMaxScaler()
+#train_data = train_data.reshape(-1,1)
+#test_data = test_data.reshape(-1,1)
+#
+## Train the Scaler with training data and smooth data
+#smoothing_window_size = 2500
+#for di in range(0,10000,smoothing_window_size):
+#    scaler.fit(train_data[di:di+smoothing_window_size,:])
+#    train_data[di:di+smoothing_window_size,:] = scaler.transform(train_data[di:di+smoothing_window_size,:])
+#
+## You normalize the last bit of remaining data
+#scaler.fit(train_data[di+smoothing_window_size:,:])
+#train_data[di+smoothing_window_size:,:] = scaler.transform(train_data[di+smoothing_window_size:,:])
+#
+## Reshape both train and test data
+#train_data = train_data.reshape(-1)
+#
+## Normalize test data
+#test_data = scaler.transform(test_data).reshape(-1)
+#
+## Now perform exponential moving average smoothing on training data
+## So the data will have a smoother curve than the original ragged data
+#EMA = 0.0
+#gamma = 0.1
+#for ti in range(11000):
+#  EMA = gamma*train_data[ti] + (1-gamma)*EMA
+#  train_data[ti] = EMA
+#
+## Used for visualization and test purposes
+#all_mid_data = np.concatenate([train_data,test_data],axis=0)
+#
+############################################################## averaging techniques
+########################################################### standard average
+#window_size = 100
+#N = train_data.size
+#std_avg_predictions = []
+#std_avg_x = []
+#mse_errors = []
+#
+#for pred_idx in range(window_size,N):
+#
+#    if pred_idx >= N:
+#        date = dt.datetime.strptime(k, '%Y-%m-%d').date() + dt.timedelta(days=1)
+#    else:
+#        date = df.loc[pred_idx,'Date']
+#
+#    std_avg_predictions.append(np.mean(train_data[pred_idx-window_size:pred_idx]))
+#    mse_errors.append((std_avg_predictions[-1]-train_data[pred_idx])**2)
+#    std_avg_x.append(date)
+#
+#print('MSE error for standard averaging: %.5f'%(0.5*np.mean(mse_errors)))
+#
+#if plot_standard_average == 1:
+#    plt.figure(figsize = (18,9))
+#    plt.plot(range(df.shape[0]),all_mid_data,color='b',label='True')
+#    plt.plot(range(window_size,N),std_avg_predictions,color='orange',label='Prediction')
+#    #plt.xticks(range(0,df.shape[0],50),df['Date'].loc[::50],rotation=45)
+#    plt.xlabel('Date')
+#    plt.ylabel('Mid Price')
+#    plt.legend(fontsize=18)
+#    plt.show()
+#
+######################################################### exponential moving average
+#window_size = 100
+#N = train_data.size
+#
+#run_avg_predictions = []
+#run_avg_x = []
+#
+#mse_errors = []
+#
+#running_mean = 0.0
+#run_avg_predictions.append(running_mean)
+#
+#decay = 0.5
+#
+#for pred_idx in range(1,N):
+#
+#    running_mean = running_mean*decay + (1.0-decay)*train_data[pred_idx-1]
+#    run_avg_predictions.append(running_mean)
+#    mse_errors.append((run_avg_predictions[-1]-train_data[pred_idx])**2)
+#    run_avg_x.append(date)
+#
+#if plot_moving_average == 1:
+#    plt.figure(figsize = (18,9))
+#    plt.plot(range(df.shape[0]),all_mid_data,color='b',label='True')
+#    plt.plot(range(0,N),run_avg_predictions,color='orange', label='Prediction')
+#    #plt.xticks(range(0,df.shape[0],50),df['Date'].loc[::50],rotation=45)
+#    plt.xlabel('Date')
+#    plt.ylabel('Mid Price')
+#    plt.legend(fontsize=18)
+#    plt.show()
+#
